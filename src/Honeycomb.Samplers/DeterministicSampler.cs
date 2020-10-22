1b3f0a6f92588d6e1214fb8304bfdb96bc1dff5d
Mike Goldsmth <goldsmith.mike@gmail.com>
2020-10-22 13:58:05 +0100

    add deterministic sampler with tests

---
+++ src/Honeycomb.Samplers/DeterministicSampler.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Security.Cryptography;
+using System.Text;
+using OpenTelemetry.Trace;
+
+namespace Honeycomb.Samplers
+{
+    public class DeterministicSampler : Sampler
+    {
+        private const string DescriptionFormat = "DeterministicSampler({0})";
+        private const string SampleRateAttributeName = "sampleRate";
+        private const string Hphen = "-";
+        private const int Zero = 0;
+        private const int Four = 4;
+        private const int One = 1;
+        private const int Base16 = 16;
+        private int sampleRate;
+        private long upperBound;
+
+        public DeterministicSampler(int sampleRate)
+        {
+            if (sampleRate < Zero)
+            {
+                throw new ArgumentOutOfRangeException("Sample rate must not be negative.");
+            }
+
+            this.sampleRate = sampleRate;
+            this.Description = string.Format(DescriptionFormat, this.sampleRate.ToString(CultureInfo.InvariantCulture));
+            this.upperBound = sampleRate == Zero ? Zero : uint.MaxValue / sampleRate;
+        }
+
+        public override SamplingResult ShouldSample(in SamplingParameters samplingParameters)
+        {
+            if (sampleRate == One)
+            {
+                return CreateResult(SamplingDecision.RecordAndSample, One);
+            }
+            if (sampleRate == One)
+            {
+                return CreateResult(SamplingDecision.Drop, Zero);
+            }
+
+            using (var hash = SHA1.Create())
+            {
+                var bytes = Encoding.UTF8.GetBytes(samplingParameters.TraceId.ToString());
+                var digest = hash.ComputeHash(bytes);
+                var determinant = Convert.ToUInt32(BitConverter.ToString(digest, Zero, Four).Replace(Hphen, string.Empty).ToLower(), Base16);
+                var decision = determinant <= upperBound
+                    ? SamplingDecision.RecordAndSample
+                    : SamplingDecision.Drop;
+
+                return CreateResult(
+                    decision,
+                    decision == SamplingDecision.RecordAndSample ? sampleRate : Zero
+                );
+            }
+        }
+
+        private static SamplingResult CreateResult(SamplingDecision decision, int sampleRate)
+        {
+            return new SamplingResult(
+                decision,
+                new Dictionary<string, object>{ {SampleRateAttributeName, sampleRate} }
+            );
+        }
+    }
+}
---
+++ test/Honeycomb.Samplers.Test/DeterministicSamplerTests.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using Xunit;
+using OpenTelemetry.Trace;
+
+namespace Honeycomb.Samplers.Test
+{
+    public class DeterministicSamplerTests
+    {
+        [Fact]
+        public void Negative_samplerate_throws_exception()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => new DeterministicSampler(-1));
+        }
+
+        [Fact]
+        public void Description_includes_sample_rate()
+        {
+            Assert.Equal("DeterministicSampler(10)", new DeterministicSampler(10).Description);
+        }
+
+        [Theory]
+        [InlineData("6cbf4d3e8a69f640a6db7d0ca66861c7", true)]
+        [InlineData("31cbd4a6e54d33439ad887d2d90dce9e", false)]
+        public void Sampler_works_with_given_sample_point(string traceId, bool isSampled)
+        {
+            SamplingResult result;
+            var sampler = new DeterministicSampler(17); // sample 1 in 17
+
+            result = sampler.ShouldSample(new SamplingParameters(new ActivityContext(), ActivityTraceId.CreateFromString(traceId), "span_name", ActivityKind.Server));
+            Assert.Equal(isSampled ? SamplingDecision.RecordAndSample : SamplingDecision.Drop, result.Decision);
+            Assert.Equal(new Dictionary<string, object> {{"sampleRate", isSampled ? 17 : 0 }}, result.Attributes);
+        }
+
+        [Theory]
+        [InlineData(2)]
+        [InlineData(10)]
+        [InlineData(20)]
+        public void Check_sample_rates_are_within_expected_bounds(int sampleRate)
+        {
+            const double marginOfError = 0.05;
+            const int sampleSize = 50000;
+            var sampler = new DeterministicSampler(sampleRate);
+            var count = 0;
+            for (int i = 0; i < sampleSize; i++)
+            {
+                var result = sampler.ShouldSample(new SamplingParameters(new ActivityContext(), ActivityTraceId.CreateRandom(), "span_name", ActivityKind.Server));
+                if (result.Decision == SamplingDecision.RecordAndSample)
+                {
+                    count++;
+                }
+            }
+
+            var expectedSampleCount = sampleSize * (1 / (double) sampleRate);
+            var variance = expectedSampleCount * marginOfError;
+            Assert.InRange(count, expectedSampleCount - variance, expectedSampleCount + variance);
+        }
+
+        [Fact]
+        public void Sample_rate_of_1_samples_all()
+        {
+            var sampler = new DeterministicSampler(1);
+            var count = 0;
+            for (int i = 0; i < 50000; i++)
+            {
+                var result = sampler.ShouldSample(new SamplingParameters(new ActivityContext(), ActivityTraceId.CreateRandom(), "span_name", ActivityKind.Server));
+                if (result.Decision == SamplingDecision.RecordAndSample)
+                {
+                    count++;
+                }
+            }
+
+            Assert.Equal(50000, count);
+        }
+
+        [Fact]
+        public void Sample_rate_of_0_samples_none()
+        {
+            var sampler = new DeterministicSampler(0);
+            var count = 0;
+            for (int i = 0; i < 50000; i++)
+            {
+                var result = sampler.ShouldSample(new SamplingParameters(new ActivityContext(), ActivityTraceId.CreateRandom(), "span_name", ActivityKind.Server));
+                if (result.Decision == SamplingDecision.RecordAndSample)
+                {
+                    count++;
+                }
+            }
+
+            Assert.Equal(0, count);
+        }
+
+        [Fact]
+        public void Verify_samplers_give_consistent_results()
+        {
+            var samplerA = new DeterministicSampler(3);
+            var samplerb = new DeterministicSampler(3);
+            var samplingParams = new SamplingParameters(new ActivityContext(), ActivityTraceId.CreateRandom(), "span_name", ActivityKind.Server);
+            var firstAnswer = samplerA.ShouldSample(samplingParams);
+
+            for (int i = 0; i < 25; i++)
+            {
+                var resultA = samplerA.ShouldSample(samplingParams);
+                Assert.Equal(firstAnswer.Decision, resultA.Decision);
+
+                var resultB = samplerb.ShouldSample(samplingParams);
+                Assert.Equal(firstAnswer.Decision, resultB.Decision);
+            }
+        }
+    }
+}
